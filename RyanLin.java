58f18ea0bd756b1bd6fae0ea803b911f51c36448
systems32 <ryanandlin@gmail.com>
2022-08-25 15:07:58 -0700

    ryanlin hobby message

--- RyanLin.java
+++ RyanLin.java
@@ -66,7 +66,7 @@ public class RyanLin extends Student implements SpecialInterestOrHobby
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to time travel!");
+                myHobby("I have been to 21 countries!");
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
